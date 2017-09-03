211177be5ee8a1d806663ab70ccc99aee172a227
Leo Heinsaar <leo.heinsaar@hotmail.com>
2017-09-03 23:12:11 +0400

    Corrected a misnaming.

--- SquareGridGraph/main.cpp
+++ SquareGridGraph/main.cpp
@@ -4,10 +4,10 @@ using namespace sgg;
 
 struct Random { int X, Y, D; };
 
-Random sec__5_d = { 150, 20, 40 }; // about 5  seconds in debug mode
+Random sec_5_d  = { 150, 20, 40 }; // about 5  seconds in debug mode
 Random sec_15_d = { 150, 40, 40 }; // about 12 seconds in debug mode
 
-auto timepack = sec__5_d;
+auto timepack = sec_15_d;
 
 int X = timepack.X;
 int Y = timepack.Y;
