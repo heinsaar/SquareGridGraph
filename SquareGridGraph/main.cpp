52938592f9bdecfb31586332c7e765ba02483473
Leo Heinsaar <leo.heinsaar@hotmail.com>
2017-09-04 13:11:33 +0400

    New timepack: r_sec_7 and r_sec_28

--- SquareGridGraph/main.cpp
+++ SquareGridGraph/main.cpp
@@ -4,10 +4,12 @@ using namespace sgg;
 
 struct Random { int X, Y, D; };
 
-Random sec_5_d  = { 150, 20, 40 }; // about 5  seconds in debug mode
-Random sec_15_d = { 150, 40, 40 }; // about 12 seconds in debug mode
+Random d_sec_5  = { 150,  20, 40 }; // about  5 seconds in debug   mode
+Random d_sec_15 = { 150,  40, 40 }; // about 12 seconds in debug   mode
+Random r_sec_7  = { 200, 200, 40 }; // about  7 seconds in release mode
+Random r_sec_28 = { 250, 250, 40 }; // about 28 seconds in release mode
 
-auto timepack = sec_15_d;
+auto timepack = r_sec_7;
 
 int X = timepack.X;
 int Y = timepack.Y;
