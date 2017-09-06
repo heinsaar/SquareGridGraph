a526d09c15307e9bb02b28ce1c9b63015a9c8f79
Leo Heinsaar <leo.heinsaar@hotmail.com>
2017-09-06 16:12:17 +0400

    Added name field to Random for Timer reports.

--- SquareGridGraph/main.cpp
+++ SquareGridGraph/main.cpp
@@ -2,12 +2,12 @@
 
 using namespace sgg;
 
-struct Random { int X, Y, D; };
+struct Random { int X, Y, D; std::string name; };
 
-Random d_sec_5  = { 150,  20, 40 }; // about  5 sec in debug   mode >  3 sec
-Random d_sec_15 = { 150,  40, 40 }; // about 12 sec in debug   mode > 12 sec
-Random r_sec_7  = { 200, 200, 40 }; // about  7 sec in release mode >  6 sec
-Random r_sec_28 = { 250, 250, 40 }; // about 28 sec in release mode > 25 sec
+Random d_sec_5  = { 150,  20, 40, "d_sec_5"  }; // about  5 sec in debug   mode >  3 sec
+Random d_sec_15 = { 150,  40, 40, "d_sec_15" }; // about 12 sec in debug   mode > 12 sec
+Random r_sec_7  = { 200, 200, 40, "r_sec_7"  }; // about  7 sec in release mode >  6 sec
+Random r_sec_28 = { 250, 250, 40, "r_sec_28" }; // about 28 sec in release mode > 25 sec
 
 auto timepack = d_sec_5;
 
@@ -21,7 +21,7 @@ int main() try
         HashDot hashdot;
         hashdot.random(timepack.X, timepack.Y, timepack.D);
         Town town(hashdot);
-        Timer<> t("d_sec_5");
+        Timer<> t(timepack.name);
         town.connect_all(true, true);
     }
     CONTINUE_ON_ENTER
