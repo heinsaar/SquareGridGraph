f4d3371d1c6731ff195c9700cb4d01ea149bd370
Leo Heinsaar <leo.heinsaar@hotmail.com>
2017-09-07 20:15:56 +0400

    Minor (spaces)

--- SquareGridGraph/grid.h
+++ SquareGridGraph/grid.h
@@ -32,7 +32,7 @@ protected:
         using Impitref  = Impit&;
         using Kernelref = Ker&;
 
-        static Impitref  to(Coord x, Coord y) { return ((Impitref) it->left);   }
+        static Impitref  to(Coord x, Coord y) { return ((Impitref) it->left);  }
         static Impitref  left(  Impit n)      { return ((Impitref) n->left);   }
         static Impitref  right( Impit n)      { return ((Impitref) n->right);  }
         static Impitref  up(    Impit n)      { return ((Impitref) n->up);     }
